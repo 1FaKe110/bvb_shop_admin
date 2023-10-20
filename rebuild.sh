971f090997863c9bef4d9c01d7487777e47ed3ca
1FaKe110 <gabko2016@gmail.com>
2023-10-20 13:08:39 +0300

    20.10 добавил build.yml для гитхаба добавил кнопку выхода из админки

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,6 @@
 #echo sudo docker build --tag bvb-shop-admin .
 sudo docker build --tag bvb-shop-admin .
 #echo sudo docker run bvb-shop -p 1111:1111 bvb-shop-admin
+sudo docker push 1fake/bvb_shop:tagname
 sudo docker run -p 1111:1111 bvb-shop-admin
+
